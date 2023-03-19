febcc954c3c5ce29a67794685586ee254a2eb267
Prateek <teeksingh@gmail.com>
2023-03-18 20:30:09 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(rideLength){
+  
+  if (rideLength <= 400){
+    return 'This one is on me!'
+  }
+
+  else if (rideLength >400 && rideLength <=2000){
+    return 'That will be twenty bucks.'
+  }
+
+  else if (rideLength > 2000 && rideLength <=2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+
+  else if (rideLength > 2500){
+    return 'No can do.'
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+    
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(charm){
+    switch(charm){
+    case 'generous':
+    return 'Thank you so much.'
+
+    case 'not as generous':
+    return 'Thank you.'
+
+    default:
+    return 'Bye.'}
 }
